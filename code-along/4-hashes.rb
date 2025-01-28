3c72ca58869972fba1119f846feb8f6dcb6b7d48
Andrew Wallen <andrew.wallen@kellogg.northwestern.edu>
2025-01-28 21:09:55 +0000

    Code alongs 2-4

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,32 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+andrew = {"name"=> "Andrew", "location"=> "Chicago","status" => "ENTR-924 at Kellogg"}
+
+puts andrew
 
 # Accessing data from the hash
+location = andrew["location"]
+puts location
 
 # More Complex Hashes
+
+my_profile = {
+    "name" => "Andrew",
+    "location" => {"city" => "Chicago", "state" => "Illinois"},
+    "status" => "Kellogg"
+}
+
+puts my_profile
+
+puts my_profile["location"]["city"]
+p my_profile ["location"]["city"]
+
+complete_profile = { "name" => "Andrew Wallen",
+                     "location" => { "city" => "Chicago", "state" => "Illinois" },
+                     "timeline" => [{ "status" => "Eating tacos", "posted" => "7:30am" },
+                                    { "status" => "Brushing teeth", "posted" => "8:00am" },
+                                    { "status" => "Eating more tacos", "posted" => "9:00am" }] }
+
+
+puts complete_profile["timeline"][0]["status"]
